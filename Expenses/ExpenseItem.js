7b68f996d46092a8f1637365d969e3d4de65e65f
kajalkapadiya <kajalkapadiya18@gmail.com>
2023-02-10 16:04:44 +0530

    add buttons for update and delete

--- Expenses/ExpenseItem.js
+++ Expenses/ExpenseItem.js
@@ -3,15 +3,33 @@ import ExpeneseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const title = props.item;
+  const desc = props.LocationOfExpenditure;
+
+  const clickEvent = () => {
+    console.log('clicked!!');
+  }
+
+  const deleteEvent = () => {
+    // const element = document.getElementsByTagName("ExpeneseDate");
+    // const element1 = document.getElementsByTagName("h2");
+    // const element2 = document.getElementsByClassName("expense-item__price");
+    // element.remove();
+    // element1.remove();
+    // element2.remove();    
+  }
+
   return (
     <Card className="expense-item">
       <ExpeneseDate date={props.date} />
       <div className="expense-item__description">
-        <h3>
-          {props.item} - {props.LocationOfExpenditure}
-        </h3>
+        <h2>
+          {title} - {desc}
+        </h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickEvent}>change title</button>
+      <button onClick={deleteEvent}>delete expense</button>
     </Card>
   );
 };
